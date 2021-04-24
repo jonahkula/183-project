3509e832412a2cb6c0d5a29151eb3ccaf3bed952
cviche <60164780+cviche@users.noreply.github.com>
2021-04-23 20:49:57 -0700

    Got the basic tables

--- models.py
+++ models.py
@@ -10,14 +10,49 @@ from pydal.validators import *
 def get_user_email():
     return auth.current_user.get('email') if auth.current_user else None
 
+
 def get_time():
     return datetime.datetime.utcnow()
 
 
-### Define your table below
+# Location Table
+db.define_table(
+    'location',
+    # Field('location_id', 'integer'),
+    Field('location_name'),
+    Field('location_address'),
+    Field('location_phone'),
+    Field('location_website')
+)
+
+# Reviews Table
+db.define_table(
+    'reviews',
+    # Field('review_id', 'integer'),
+    Field('location_id', 'reference location'),
+    Field('user_id', 'reference auth_user'),
+    Field('reviews_message'),
+    Field('reviews_user_rating'),
+    Field('reviews_message_rating'),
+    Field('reviews_threads', 'integer')
+)
+
+# Saved Locations Table
+db.define_table(
+    'saved_locations',
+    # Field('saved_locations_id', 'integer'),
+    Field('user_id', 'reference auth_user'),
+    Field('saved_locations_name'),
+    Field('saved_locations_address'),
+    Field('saved_locations_zipcode'),
+    Field('saved_locations_radius'),
+)
+
+
+# Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
-## always commit your models to avoid problems later
+# always commit your models to avoid problems later
 
 db.commit()
